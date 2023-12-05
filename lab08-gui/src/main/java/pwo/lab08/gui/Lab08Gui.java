6c3741409dcb14345d49b87e3e1b911b98fb655c
szyme <szyme@DESKTOP-KG1ODI8>
2023-12-05 02:49:25 +0100

    Komentarze

---
+++ lab08-gui/src/main/java/pwo/lab08/gui/Lab08Gui.java
@@ -0,0 +1,63 @@
+package pwo.lab08.gui;
+
+import pwo.lab08.engine.Engine;
+
+public class Lab08Gui extends javax.swing.JFrame {
+
+    private static final String APP_NAME = "Lab08 GUI Application ";
+    private static int DEFAULT_WIDHT = 1000, DEFAULT_HEIGHT = 500;
+
+    public Lab08Gui() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jMenu1.setText("File");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 279, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                Lab08Gui lab08Gui = new Lab08Gui();
+                lab08Gui.setTitle(APP_NAME + Engine.getVersion());
+                lab08Gui.setSize(DEFAULT_WIDHT, DEFAULT_HEIGHT);
+                lab08Gui.setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    // End of variables declaration//GEN-END:variables
+}
